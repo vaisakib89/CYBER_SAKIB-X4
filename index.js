2c9e0ac1ed2eb86c01e95551647af63b33ef665a
vaisakib89 <vaisakib499@gmail.com>
2025-09-07 08:50:57 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-require ('./main/catalogs/IMRANA.js')
+// মূল কনফিগ/লগার ফাইল লোড
+require('./main/catalogs/IMRANA.js');
+
+// mentionReply লিংক
+const mentionReply = require('./main/events/mentionReply.js');
+
+// api.listenMqtt বা মেসেজ হ্যান্ডলার যদি IMRANA.js এ থাকে,
+// তাহলে সেখানে event পাঠাতে হবে
+// উদাহরণ: যদি IMRANA.js exports api থাকে
+const { api } = require('./main/catalogs/IMRANA.js');
+
+api.listenMqtt(async (err, event) => {
+  if (err) return console.error(err);
+
+  // mentionReply চালু করা
+  mentionReply({ api })(event);
+});
