7c6169de165ef59e99794fd50c5e657d4f0943e8
vaisakib89 <vaisakib499@gmail.com>
2025-09-07 09:45:15 +0600

    Update mentionReply.js

--- scripts/commands/mentionReply.js
+++ scripts/commands/mentionReply.js
@@ -11,17 +11,12 @@ module.exports.config = {
 
 module.exports = async function ({ event, api }) {
     try {
-        // শুধুমাত্র এই UID মেনশন হলে রিপ্লাই
         const targetUIDs = ["100090445581185"];
-
         if (event.type === "message" && event.mentions) {
             const mentionedIDs = Object.keys(event.mentions);
             const found = targetUIDs.find(uid => mentionedIDs.includes(uid));
-
             if (found) {
                 let name = event.mentions[found] || "Special User";
-
-                // Send reply
                 api.sendMessage(
                     `⚡ তুমি ${name} কে মেনশন করেছো! সাবধানে কথা বলো!`,
                     event.threadID,
