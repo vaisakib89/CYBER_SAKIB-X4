91a8f8ec2ed02b03be9c2340fdc1a82bb8c2e5ad
vaisakib89 <vaisakib499@gmail.com>
2025-09-17 00:44:02 +0600

    Update hack.js

--- scripts/commands/hack.js
+++ scripts/commands/hack.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "hack", 
   version: "1.0.0", 
   permission: 0,
-  credits: "SAKIB",
+  credits: "Nayan",
   description: "example",
   prefix: true,
   category: "Fun", 
@@ -101,4 +101,4 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
       event.threadID,
       () => fs.unlinkSync(pathImg),
       event.messageID);
-                                      }
+	}
