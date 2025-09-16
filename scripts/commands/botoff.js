6c4c92f1067b8fab07552153462555db79cbbff1
vaisakib89 <vaisakib499@gmail.com>
2025-09-16 21:54:32 +0600

    Update botoff.js

--- scripts/commands/botoff.js
+++ scripts/commands/botoff.js
@@ -5,7 +5,7 @@ module.exports.config = {
   name: "botoff",
   version: "1.0.0",
   permission: 2, // 2 = Admin, Operator, or Owner
-  credits: "YourName",
+  credits: "SAKIB",
   description: "Turn the bot OFF",
   prefix: true,
   category: "system",
