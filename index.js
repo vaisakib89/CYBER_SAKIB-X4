6490d4f3e88e787b89f0984a94db7a1772d74de6
♕ 𝐒𝐀𝐊𝐈𝐁 ♕ <vaisakib499@gmail.com>
2025-10-24 01:38:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Main Bot File (index.js)
+require('./deployLogColor.js');
 require('./scripts/deployLogColor'); // <-- এটা বসাও
 require('./main/catalogs/website/checkName.js');
 require('./main/catalogs/IMRANA.js');
