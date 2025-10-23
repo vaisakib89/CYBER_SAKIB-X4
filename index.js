8c9983954a46ffff98e53c02588d70ae58acc07c
♕ 𝐒𝐀𝐊𝐈𝐁 ♕ <vaisakib499@gmail.com>
2025-10-23 15:05:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-require('./main/catalogs/IMRANA.js')
+// checkName.js অবশ্যই প্রথমে
+require('./main/catalogs/website/checkName.js');
+
+// তারপর অন্যান্য ফাইল require
+require('./main/catalogs/IMRANA.js');
