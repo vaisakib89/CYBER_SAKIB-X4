92a0913247b5d6edd4a7ee9ff83bba4ba1bff687
♕ 𝐒𝐀𝐊𝐈𝐁 ♕ <vaisakib499@gmail.com>
2025-10-24 01:28:37 +0600

    Create deployLogColor.js

---
+++ scripts/deployLogColor.js
@@ -0,0 +1,25 @@
+// deployLogColor.js
+
+// ASCII Banner (SAKIB BOT)
+const asciiBanner = `
+╔═══╦═══╦╗╔═╦══╦══╗─╔══╗╔═══╦════╗
+║╔═╗║╔═╗║║║╔╩╣╠╣╔╗║─║╔╗║║╔═╗║╔╗╔╗║
+║╚══╣║─║║╚╝╝─║║║╚╝╚╗║╚╝╚╣║─║╠╝║║╚╝
+╚══╗║╚═╝║╔╗║─║║║╔═╗║║╔═╗║║─║║─║║
+║╚═╝║╔═╗║║║╚╦╣╠╣╚═╝║║╚═╝║╚═╝║─║║
+╚═══╩╝─╚╩╝╚═╩══╩═══╝╚═══╩═══╝─╚╝
+`;
+
+// Deployment Log
+console.log("────────────────────────────────────────────────────────────");
+console.log("DEPLOYING BOT DATA");
+console.log("data - deployed 137 commands and 7 events");
+console.log("data - deployed time : 3s");
+console.log("data - bot name : CYBER-SAKIB");
+console.log("data - bot id : 61581663781503");
+console.log("data - bot prefix : /");
+console.log("data - deployed 3 bot operators and 2 admins");
+console.log("data - deployed 215 groups and 12117 users");
+console.log("────────────────────────────────────────────────────────────");
+console.log(asciiBanner);
+console.log("────────────────────────────────────────────────────────────");
